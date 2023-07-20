aad3d19c34484b2f4851004ad70f24ee5144d5ac
Donovan Vernon <44986610+dvernon5@users.noreply.github.com>
2023-07-19 22:46:02 -0700

    Added a routine to check availability
    In the following routine I am checking three important validations. One is checking row and making sure there is no duplicate number. Secondly checking column and making sure there is no duplicate number. Lastly, checking each 3X3 and making sure there is no duplicate number.

--- sudoku_game.cpp
+++ sudoku_game.cpp
@@ -12,9 +12,37 @@ bool findEmptyCell(int board[BOARD_SIZE][BOARD_SIZE], int& row, int& col)
         {
             if (board[row][col] == EMPTY_CELL)
             {
-                return true;
+                return true;  // Found an empty cell.
             }
         }
     }
-    return false;
+    return false;  // No empty cell found.
+}
+
+bool spaceAvailable(int board[BOARD_SIZE][BOARD_SIZE], int row, int col, int num)
+{
+    // Check if num is already present in the same row or column
+    for (int i = 0; i < BOARD_SIZE; ++i)
+    {
+        if (board[row][i] == num || board[i][col] == num)
+        {
+            return false;
+        }
+    }
+
+    // Check if num is already present in the same 3X3 board.
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;
+    
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            if (board[startRow + i][startCol + j] == num)
+            {
+                return false;
+            }
+        }
+    }
+    return true;  // If num doesn't conflict with any other number, space is available. 
 }
